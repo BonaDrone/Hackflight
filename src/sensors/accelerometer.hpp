5544b2798216f7cfef0337caf47abfc47bae6dfa
Juan Gallostra <juangallostra@gmail.com>
2019-01-28 11:07:16 +0100

    Fix observation jacobian indexes

--- src/sensors/accelerometer.hpp
+++ src/sensors/accelerometer.hpp
@@ -52,64 +52,64 @@ namespace hf {
             {
               // 1 column
               H[0] =  0;
-              H[3] =  0;
-              H[6] =  0;
+              H[15] =  0;
+              H[30] =  0;
               // 2 column
               H[1] =  0;
-              H[4] =  0;
-              H[7] =  0;
+              H[16] =  0;
+              H[31] =  0;
               // 3 column
               H[2] =  0;
-              H[5] =  0;
-              H[8] =  0;
+              H[17] =  0;
+              H[32] =  0;
               // 4 column
               H[3] =  0;
-              H[6] =  0;
-              H[9] =  0;
+              H[18] =  0;
+              H[33] =  0;
               // 5 column
               H[4] =  0;
-              H[7] =  0;
-              H[10] =  0;
+              H[19] =  0;
+              H[34] =  0;
               // 6 column
               H[5] =  0;
-              H[8] =  0;
-              H[11] =  0;
+              H[20] =  0;
+              H[35] =  0;
               // 7 column
               H[6] =                                  0;
-              H[9] =  x[6]*x[6] - x[7]*x[7] - x[8]*x[8] + x[9]*x[9];
-              H[12] =            - 2*x[6]*x[7] - 2*x[8]*x[9];
+              H[21] =  x[6]*x[6] - x[7]*x[7] - x[8]*x[8] + x[9]*x[9];
+              H[36] =            - 2*x[6]*x[7] - 2*x[8]*x[9];
               // 8 column
               H[7] =  - x[6]*x[6] + x[7]*x[7] + x[8]*x[8] - x[9]*x[9];
-              H[10] =                                    0;
-              H[13] =                2*x[7]*x[9] - 2*x[6]*x[8];
+              H[22] =                                    0;
+              H[37] =                2*x[7]*x[9] - 2*x[6]*x[8];
               // 9 column
               H[8] =  2*x[6]*x[7] + 2*x[8]*x[9];
-              H[11] =  2*x[6]*x[8] - 2*x[7]*x[9];
-              H[14] =                      0;
+              H[23] =  2*x[6]*x[8] - 2*x[7]*x[9];
+              H[38] =                      0;
               // 10 column
               H[9] =  0;
-              H[12] =  0;
-              H[15] =  0;
+              H[24] =  0;
+              H[39] =  0;
               // 11 column
               H[10] =  0;
-              H[13] =  0;
-              H[16] =  0;
+              H[25] =  0;
+              H[40] =  0;
               // 12 column
               H[11] =  0;
-              H[14] =  0;
-              H[17] =  0;
+              H[26] =  0;
+              H[41] =  0;
               // 13 column
               H[12] =  0;
-              H[15] =  0;
-              H[18] =  0;
+              H[27] =  0;
+              H[42] =  0;
               // 14 column
               H[13] =  0;
-              H[16] =  0;
-              H[19] =  0;
+              H[28] =  0;
+              H[43] =  0;
               // 15 column
               H[14] =  0;
-              H[17] =  0;
-              H[20] =  0;
+              H[29] =  0;
+              H[44] =  0;
             }
 
             virtual void getInnovation(float * z, float * x) override
@@ -118,17 +118,12 @@ namespace hf {
                 float tmp2[3];
                 // We might have to normalize these two vectors (y and h)
                 // Predicted Observations
-                _predictedObservation[0] = ((2*x[6]*x[8] - 
-                                            2*x[7]*x[9])*-9.80665);
-                _predictedObservation[1] = ((-2*x[6]*x[7] - 
-                                             2*x[8]*x[9])*-9.80665);
-                _predictedObservation[2] = ((-x[6]*x[6] + 
-                                             x[7]*x[7] +
-                                             x[8]*x[8] -
-                                             x[9]*x[9])*-9.80665);
+                _predictedObservation[0] = ((2*x[6]*x[8] - 2*x[7]*x[9])*-9.80665);
+                _predictedObservation[1] = ((-2*x[6]*x[7] - 2*x[8]*x[9])*-9.80665);
+                _predictedObservation[2] = ((-x[6]*x[6] + x[7]*x[7] + x[8]*x[8] - x[9]*x[9]) * -9.80665);
                                              
-                norvec(_predictedObservation, tmp1, 3);
-                norvec(_accels, tmp2, 3);
+                //norvec(_predictedObservation, tmp1, 3);
+                //norvec(_accels, tmp2, 3);
                 // innovation = measured - predicted
                 z[0] = tmp2[0] - tmp1[0];
                 z[1] = tmp2[1] - tmp1[1];
