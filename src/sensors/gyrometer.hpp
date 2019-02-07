d9a2aaf018fe5222c679476d692587b1e6b49ee6
Juan Gallostra <juangallostra@gmail.com>
2019-02-07 18:27:45 +0100

    Do not change sign of rates

--- src/sensors/gyrometer.hpp
+++ src/sensors/gyrometer.hpp
@@ -51,9 +51,9 @@ namespace hf {
                 (void)time;
 
                 // NB: We negate gyro X, Y to simplify PID controller
-                state.angularVelocities[0] =  _rates[0];
-                state.angularVelocities[1] = -_rates[1];
-                state.angularVelocities[2] = -_rates[2];
+                state.angularVelocities[0] = _rates[0];
+                state.angularVelocities[1] = _rates[1];
+                state.angularVelocities[2] = _rates[2];
                 
             }
 
